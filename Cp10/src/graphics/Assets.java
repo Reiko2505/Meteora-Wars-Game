410e1ffd776fda71fe2e9ce2d254c9fe54532fa6
MrRaccoot <fadeddiane@gmail.com>
2025-05-19 11:35:26 -0600

    ✨Capítulo 10
    Se añade el archivo "Laser" que esta ligado al asset de laser, para que la navecita pueda disparar 💫

---
+++ Cp10/src/graphics/Assets.java
@@ -0,0 +1,26 @@
+package graphics;
+
+import java.awt.image.BufferedImage;
+
+public class Assets {
+	
+	public static BufferedImage player;
+	
+	public static BufferedImage speed;
+
+    public static BufferedImage blueLaser, greenLaser, redLaser;
+	
+	public static void init()
+	{
+		player = Loader.ImageLoader("/res/ships/witchspaceship.png");
+		
+		speed = Loader.ImageLoader("/res/effects/firemagic.png");
+
+        blueLaser = Loader.ImageLoader("/res/lasers/laserBlue01.png");
+		
+		greenLaser = Loader.ImageLoader("/res/lasers/laserGreen11.png");
+		
+		redLaser = Loader.ImageLoader("/res/lasers/laserRed01.png");
+	}
+	
+}
